659b5cbfa12b9203950256a6e38d7afc6a6836cf
Alexander-Andrade <veimar.94@gmail.com>
2017-10-13 14:38:15 +0300

    add check

--- main.rb
+++ main.rb
@@ -2,12 +2,20 @@ class Grammar
 
   RULE = /^(?<left>[a-z]+)->(?<right>[a-zλ]+(\|[a-zλ]+)*)$/i
 
+  attr_accessor :rules
+
   def initialize(lines)
     @lines = lines
     @rules = Hash.new { |hash, key|  hash[key] = Array.new }
     trim
     raise StandardError, "Given grammar contains errors" unless valid?
     parse
+  end
+
+
+  def classify
+    info = {class: 0, name: 'unrestricted grammar'}
+
   end
 
   private
@@ -26,8 +34,17 @@ class Grammar
       left = match_data[:left]
       right = match_data[:right].split('|')
       @rules[left].push(*right)
-      puts("left #{left}, right #{right} rule #{@rules[left]}")
     end
+  end
+
+  alias :valid? :unrestricted?
+  
+  def noncontracting?
+    @rules.all? { |left, right| left.length <= right.length }
+  end
+
+  def klass_2?
+
   end
 
 end
@@ -36,6 +53,7 @@ begin
   lines = IO.readlines(ARGV[0])
 
   grammar = Grammar.new(lines)
+  puts(grammar.rules)
 rescue => e
   puts e
   exit
