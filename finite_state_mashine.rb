414ac43807d5c70b697061606a8ff133adb29fb4
Alexander-Andrade <veimar.94@gmail.com>
2017-10-19 21:38:36 +0300

    fill up F set

--- finite_state_mashine.rb
+++ finite_state_mashine.rb
@@ -14,8 +14,6 @@ class FSM
   def initialize(params)
     @state_transition_file = params[:state_transition_file]
 
-    @Q = params[:Q]
-    @T = params[:T]
     @F = Hash.new { |hash, key| hash[key] = Hash.new }
     @H = params[:H]
     @Z = params[:Z]
@@ -33,12 +31,20 @@ class FSM
     puts "Z: #{@Z}"
   end
 
+  private
+
   def state_transition_function_from_csv
     state_transition = CSV.read('state_transition_function.csv',
                                 headers:true, col_sep: ' ')
 
-    puts state_transition.headers
-    puts state_transition['F']
+    @Q = Marshal.load(Marshal.dump(state_transition.headers))[1..-1]
+    @T = Marshal.load(Marshal.dump(state_transition['F']))
+
+    @Q.each do |nonterm|
+      @T.each_with_index do |term, i|
+        @F[nonterm][term] = state_transition[nonterm][i]
+      end
+    end
   end
 
 end
--- ppta3.rb
+++ ppta3.rb
@@ -3,17 +3,12 @@ require_relative 'finite_state_mashine'
 
 begin
   fsm = FSM.new({
-                  Q: %w(X Y Z I J K L N M),
-                  T: %w(n m i j ! /),
                   H: %w(X),
                   Z: %w(N),
                   state_transition_file: 'state_transition_function.csv'
                 })
 
-  state_transition.ea do |row|
-    puts row["A"]
-  end
-
+  fsm.console
 rescue => e
   puts e
   exit
