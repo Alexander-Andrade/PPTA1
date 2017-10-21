6366c327bf576e1cefe5b28e77b04d7fe85efcd1
Alexander-Andrade <veimar.94@gmail.com>
2017-10-21 16:40:34 +0300

    add result output

--- ppta3.rb
+++ ppta3.rb
@@ -1,22 +1,19 @@
 require 'csv'
 require_relative 'finite_state_mashine'
 
-# begin
-  fsm = FSM.new({
-                  H: "X",
-                  Z: %w(N),
-                  state_transition_file: 'state_transition_function.csv'
-                })
+fsm = FSM.new({
+                H: "X",
+                Z: %w(N),
+                state_transition_file: 'state_transition_function.csv'
+              })
 
-  fsm.console
-  fsm.output("graph")
+fsm.console
+fsm.output("graph")
 
-  2.times{ puts; }
+2.times{ puts; }
 
-  fsm.minimize
-  fsm.console
-  fsm.output("minimized_graph")
-# rescue => e
-#   puts e
-#   exit
-# end
+fsm.minimize
+fsm.console
+fsm.output("minimized_graph")
+puts "partition: #{fsm.partition}"
+puts "group states map: #{fsm.group_states_map}"
