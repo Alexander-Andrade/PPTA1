4482cd89fb40f43431d451925822eba1c2850ebf
Alexander-Andrade <veimar.94@gmail.com>
2017-11-01 22:12:24 +0300

    grammar input

--- ppta2.rb
+++ ppta2.rb
@@ -1,17 +1,12 @@
+require 'json'
 require_relative 'grammar'
 require_relative 'finite_state_mashine'
 
 # G=(T, N, P, S)
 # G=({X, Y, Z, W, V}, {0, 1, ~, #, &}, P, X)
+grammar_definition = JSON.load File.new("regular_grammar.json")
 
-raw_rules = IO.readlines("regular_grammar.txt")
-
-grammar = Grammar.new({
-                          T: %w(0 1 ~ # &),
-                          N: %w(X Y Z W V),
-                          P: raw_rules,
-                          S: "X"
-                      })
+grammar = Grammar.new(grammar_definition)
 
 nfa = NFA.new(grammar)
 puts "is deterministic? #{nfa.deterministic?}"
